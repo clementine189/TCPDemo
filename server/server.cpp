ae290a7963ac0e4a9b8dfa7b7871bbad02fb1e32
clementineMT <18955524883>
2022-01-06 15:23:25 +0800

    v1.3(server select)

--- server/server.cpp
+++ server/server.cpp
@@ -7,6 +7,9 @@
 #include <arpa/inet.h>
 #include <sys/ioctl.h>
 #include <errno.h>
+#include <vector>
+#include <algorithm>
+
 enum  CMD
 {
     CMD_LOGIN,
@@ -70,7 +73,57 @@ struct LogoutResult:public DataHeader
     int result;
 };
 
+//客户端socket
+std::vector<int> g_clients;
 
+int processor(int clientsockfd)
+{
+    //5.recv 
+    DataHeader header = {};
+    int nLen = recv(clientsockfd, (char *)&header, sizeof(DataHeader), 0);
+    if(nLen <= 0)
+    {
+        printf("客户端已退出，任务结束.\n");
+        //有客户端退出，返回-1，交给主函数处理
+        return -1;
+        
+    }
+    switch(header.cmd)
+    {
+        case CMD_LOGIN:
+            {
+                Login login = {};
+                recv(clientsockfd, (char *)&login+sizeof(DataHeader), sizeof(Login)-sizeof(DataHeader), 0);
+                printf("收到命令: CMD_LOGIN 数据长度: %d UerName: %s PassWord: %s\n", login.dataLength, login.userName, login.PassWord);
+                //忽略判断用户信息的正确性
+                LoginResult ret;
+                // send(clientsockfd, (const char *)&header, sizeof(DataHeader), 0);
+                send(clientsockfd, (const char *)&ret, sizeof(LoginResult), 0);             
+                break;
+            }         
+
+        case CMD_LOGOUT:            
+            {
+                Logout logout = {};
+                recv(clientsockfd, (char *)&logout+sizeof(DataHeader), sizeof(Logout)-sizeof(DataHeader), 0);
+                printf("收到命令: CMD_LOGOUT 数据长度: %d UserName: %s\n", logout.dataLength, logout.userName);
+                //忽略判断用户信息的正确性
+                LogoutResult ret;
+                //  send(clientsockfd, (const char *)&header, sizeof(DataHeader), 0);
+                send(clientsockfd, (const char *)&ret, sizeof(LogoutResult), 0);            
+                break;
+            }
+        default:            
+            {                
+                header.cmd = CMD::CMD_ERROR;
+                header.dataLength = 0;
+                send(clientsockfd, (const char *)&header, sizeof(DataHeader), 0);
+                
+                break;
+            }
+    }
+    return 0;               
+}
 
 int main()
 {
@@ -83,6 +136,7 @@ int main()
     server_address.sin_port = htons(port);
     //1、建立一个socket
     int sockfd = socket(PF_INET, SOCK_STREAM, 0);
+    int maxfd = sockfd;
     //2、bind用于接收客户端的端口号
     if(bind(sockfd, (struct sockaddr *)&server_address, sizeof(sockaddr_in)) < 0)
     {
@@ -101,80 +155,73 @@ int main()
     {
         printf("监听成功！\n");
     }
-    //4、accept
-    struct sockaddr_in clientAddr = {};
-    socklen_t nAddrLen = sizeof(sockaddr_in);
-    
-    
-        int clientsockfd = accept(sockfd, (struct sockaddr *)&clientAddr, &nAddrLen);
-        if(clientsockfd < 0)
-        {
-            printf("错误，接受到无效客户端！\n");
-        }
-        printf("新客户端加入：IP = %s \n", inet_ntoa(clientAddr.sin_addr));
+
     
     while(true)
     {
-        //5.recv 
-        DataHeader header = {};
-        //char recvBuf[128] = {};
-        int nLen = recv(clientsockfd, (char *)&header, sizeof(DataHeader), 0);
-        if(nLen < 0)
+        fd_set fdRead;
+        fd_set fdWrite;
+        fd_set fdExp;
+
+        FD_ZERO(&fdRead);
+        FD_ZERO(&fdWrite);
+        FD_ZERO(&fdExp);
+
+        FD_SET(sockfd, &fdRead);
+        FD_SET(sockfd, &fdWrite);
+        FD_SET(sockfd, &fdExp);
+
+        for(int i = 0; i < g_clients.size(); ++i)
         {
-            printf("客户端已退出，任务结束.");
+            //把这些客户端放入可读事件查询集中
+            FD_SET(g_clients[i], &fdRead);
         }
-        // printf("收到命令： %d 数据长度： %d\n", header.cmd, header.dataLength);
-        switch(header.cmd)
+        
+        //非阻塞模式
+        timeval t = {0, 0};
+        //nfds是一个整数值，是指fd_set集合中所有描述符
+        int ret = select(maxfd + 1, &fdRead, &fdWrite, &fdExp, &t);
+        if(ret < 0)
         {
-            case CMD_LOGIN:
+            printf("select任务结束。 \n");
+            break;
+        }
+        //可读事件
+        if(FD_ISSET(sockfd, &fdRead))
+        {
+            FD_CLR(sockfd, &fdRead);
+            //4、accept
+            struct sockaddr_in clientAddr = {};
+            socklen_t nAddrLen = sizeof(sockaddr_in);
+            int clientsockfd = accept(sockfd, (struct sockaddr *)&clientAddr, &nAddrLen);
+            if(maxfd < clientsockfd)
+            {
+                maxfd = clientsockfd;
+            }
+            if(clientsockfd < 0)
+            {
+                printf("错误，接受到无效客户端！\n");
+            }
+            g_clients.push_back(clientsockfd);
+            printf("新客户端加入：IP = %s \n", inet_ntoa(clientAddr.sin_addr));  
+        }
+        for(int i = 0; i <= maxfd; ++i)
+        {
+
+            if(FD_ISSET(i, &fdRead)&&(-1 == processor(i)))
+            {
+                auto iter = std::find(g_clients.begin(), g_clients.end(), i);
+                if(iter != g_clients.end())
                 {
-                    Login login = {};
-                    recv(clientsockfd, (char *)&login+sizeof(DataHeader), sizeof(Login)-sizeof(DataHeader), 0);
-                    printf("收到命令: CMD_LOGIN 数据长度: %d UerName: %s PassWord: %s\n", login.dataLength, login.userName, login.PassWord);
-                    //忽略判断用户信息的正确性
-                    LoginResult ret;
-                    // send(clientsockfd, (const char *)&header, sizeof(DataHeader), 0);
-                    send(clientsockfd, (const char *)&ret, sizeof(LoginResult), 0);             
-                    break;
-                }         
-
-            case CMD_LOGOUT:            
-                    {
-                        Logout logout = {};
-                        recv(clientsockfd, (char *)&logout+sizeof(DataHeader), sizeof(Logout)-sizeof(DataHeader), 0);
-                        printf("收到命令: CMD_LOGOUT 数据长度: %d UserName: %s\n", logout.dataLength, logout.userName);
-                        //忽略判断用户信息的正确性
-                        LogoutResult ret;
-                        //  send(clientsockfd, (const char *)&header, sizeof(DataHeader), 0);
-                        send(clientsockfd, (const char *)&ret, sizeof(LogoutResult), 0);            
-                        break;
-                    }
-            default:            
-                    {                
-                        header.cmd = CMD::CMD_ERROR;
-                        header.dataLength = 0;
-                        send(clientsockfd, (const char *)&header, sizeof(DataHeader), 0);
-                    
-                        break;
-                    }
-
+                    g_clients.erase(iter);
+                }
+            }
         }
-        // //6、处理请求
+    }
 
-        // if(0 == strcmp(recvBuf, "getInfo"))
-        // {
-        //     printf("收到命令：%s\n", recvBuf);
-        //     DataPackage dp = {27, "clementine"};
-            
-        //     send(clientsockfd, (const char *)&dp, sizeof(DataPackage), 0);
-        // }
-        // else
-        // {
-        //     printf("收到无效命令： %s\n", recvBuf);
-        //     char msgBuf[] = "???";
-        //     send(clientsockfd, msgBuf, strlen(msgBuf)+1, 0); 
-        // }
-               
+    for(int i = 0; i < g_clients.size(); ++i)
+    {
+        close(g_clients[i]);
     }
 
     //7、close
